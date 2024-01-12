0a995cc021eebe79e2e735a05d1efa1e24a39f34
Nathenial <113154849+nathenialalleyne@users.noreply.github.com>
2024-01-12 02:22:37 -0500

    test fix for testing

--- next.config.mjs
+++ next.config.mjs
@@ -6,14 +6,16 @@ await import("./src/env.mjs");
 
 /** @type {import("next").NextConfig} */
 const config = {
-  reactStrictMode: true,
-  output: standalone,
+  reactStrictMode: true, 
   /**
    * If you have `experimental: { appDir: true }` set, then you must comment the below `i18n` config
    * out.
    *
    * @see https://github.com/vercel/next.js/issues/41980
    */
+  experimental: {
+    outputStandalone: true,
+  },
   i18n: {
     locales: ["en"],
     defaultLocale: "en",
